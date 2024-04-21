2c89b122a95f66fc7a6b380638ff3b541e64ebb3
RekaSzuromi <szuromir2@gmail.com>
2024-04-21 21:33:55 +0200

    polygon change

--- main.js
+++ main.js
@@ -15,14 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         // Define the polygon coordinates
         const polygon = [
-            {x: 182, y: 679}, {x: 160, y: 690}, {x: 134, y: 725}, {x: 140, y: 767},
-            {x: 149, y: 738}, {x: 148, y: 773}, {x: 148, y: 781}, {x: 118, y: 797},
-            {x: 109, y: 815}, {x: 113, y: 845}, {x: 116, y: 868}, {x: 123, y: 891},
-            {x: 132, y: 914}, {x: 121, y: 953}, {x: 261, y: 949}, {x: 257, y: 921},
-            {x: 279, y: 883}, {x: 289, y: 837}, {x: 291, y: 806}, {x: 255, y: 779},
-            {x: 261, y: 753}, {x: 258, y: 726}, {x: 269, y: 740}, {x: 267, y: 721},
-            {x: 253, y: 697}, {x: 241, y: 689}, {x: 265, y: 691}, {x: 250, y: 681},
-            {x: 227, y: 681}, {x: 211, y: 687}
+            {x: 182, y: 679}, {x: 160, y: 690}
         ];
 
         // Set composite mode to 'destination-in' to keep only the pixels that overlap both the existing canvas and the new path
