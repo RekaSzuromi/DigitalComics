7bd395e31b1705028052d6cbf122787762660ec2
RekaSzuromi <szuromir2@gmail.com>
2024-04-29 11:12:03 +0200

    emotion assosiation to panel

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+
+function pointInPolygon(point, polygon) {
+    var x = point.x, y = point.y;
+    var inside = false;
+    for (var i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+        var xi = polygon[i].x, yi = polygon[i].y;
+        var xj = polygon[j].x, yj = polygon[j].y;
+        var intersect = ((yi > y) != (yj > y))
+            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        if (intersect) inside = !inside;
+    }
+    return inside;
+}
+
 // Function to draw the panel on the canvas, adjusted to set canvas size dynamically and without outline
 function drawPanel(ctx, image, vertices, isRectangle) {
     let minX, maxX, minY, maxY;
@@ -38,27 +52,53 @@ function drawPanel(ctx, image, vertices, isRectangle) {
 }
 
 // Process the panel data and create canvases
-function processPanelData(panelData) {
+function processPanelData(panelData, emotionData) {
     panelData.forEach((panel, index) => {
         let canvas = document.createElement('canvas');
         canvas.id = `panelCanvas-${index}`;
-        canvas.style.marginBottom = "40px"; // Adds space between each canvas
+        canvas.style.border = "1px solid black";
+        canvas.style.marginBottom = "10px";
         document.body.appendChild(canvas);
-        
+
         let ctx = canvas.getContext('2d');
         let image = new Image();
         image.src = `pages/page-${panel['Page Number']}.jpg`;
 
         image.onload = function() {
-            let vertices = panel['Panel Region Vertices']
+            let panelVertices = panel['Panel Region Vertices']
                 .match(/\((\d+,\d+)\)/g)
                 .map(s => s.replace(/[()]/g, '').split(',').map(Number))
                 .map(([x, y]) => ({x, y}));
 
-            // Determine if the panel is a rectangle
-            let isRectangle = vertices.length === 2;
+            drawPanel(ctx, image, panelVertices, panelVertices.length > 2);
 
-            drawPanel(ctx, image, vertices, isRectangle);
+            // Process emotions for this panel
+            let panelEmotions = emotionData.filter(emotion => emotion['Page Number'] === panel['Page Number']);
+            let emotionTexts = [];
+
+            panelEmotions.forEach(emotion => {
+                let emotionVertices = emotion['Emotion Region Vertices']
+                    .match(/\((\d+,\d+)\)/g)
+                    .map(s => s.replace(/[()]/g, '').split(',').map(Number))
+                    .map(([x, y]) => ({x, y}));
+                
+                // Count how many vertices of the emotion are inside the panel
+                let countInside = emotionVertices.reduce((count, vertex) => count + pointInPolygon(vertex, panelVertices), 0);
+                
+                // If more than half of the vertices are inside the panel, consider the emotion associated with this panel
+                if (countInside > emotionVertices.length / 2) {
+                    emotionTexts.push(emotion['Taxonomy Path']);
+                }
+            });
+
+            // Create a div to show associated emotions next to the canvas
+            if (emotionTexts.length) {
+                let taxonomyDiv = document.createElement('div');
+                taxonomyDiv.innerText = emotionTexts.join('\n');
+                taxonomyDiv.style.whiteSpace = 'pre';
+                taxonomyDiv.style.marginLeft = '10px';
+                document.body.appendChild(taxonomyDiv);
+            }
         };
     });
 }
